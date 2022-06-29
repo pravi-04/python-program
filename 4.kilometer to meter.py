c5379b97705b3508a7a6c6fd343c0215c17f1826
pravi-04 <107837633+pravi-04@users.noreply.github.com>
2022-06-29 16:09:52 +0530

    Add files via upload

---
+++ 4.kilometer to meter.py
@@ -0,0 +1,4 @@
+a=int(input("enter value(in kilometers)"));
+cov=0.62137
+b=a*cov
+print("value entered in miles", b);
---
+++ 5.number checking.py
@@ -0,0 +1,8 @@
+a=int(input("Enter any number"));
+if(a>0):
+    print("The entered number is positive")
+if(a<0):
+    print("The entered number is negative")
+else:
+    print("The number is 0")
+
